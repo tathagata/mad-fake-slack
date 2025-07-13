ae336371b0dc40fc10c80272a89ad9fecd09d992
Tathagata Dasgupta <tathagatadg@gmail.com>
2025-07-13 09:27:36 -0400

    Added characters needed for the slack conversation

--- db/messages/index.js
+++ db/messages/index.js
@@ -4,7 +4,7 @@ const modules = {};
 require('fs').readdirSync(__dirname).forEach((file) => {
   if (file.match(/\.json$/) !== null && file !== 'index.js') {
     const name = file.replace('.json', '');
-    modules[name] = JSON.parse(fs.readFileSync(path.join(__dirname, file)));
+    modules[name] = JSON.parse(fs.readFileSync(path.join(__dirname, file), 'utf8'));
   }
 });
 
